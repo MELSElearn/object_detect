1a815856e5665044932d5232851800bb41b021d1
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-03-16 14:13:43 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,9 +6,9 @@ import cv2
 from base64 import b64decode, b64encode
 from cvzone.HandTrackingModule import HandDetector
 from pyzbar.pyzbar import decode
+import tensorflow as tf
 from tensorflow import keras
 
-model = tensorflow.keras.models.load_model('Model/keras_model.h5')
 
 app = Flask(__name__)
 
